44ee354b5b836c43deb8096ebaeaf8b6cacc2983
stephaniepuertas <puertasstephanie@gmail.com>
2023-04-03 14:38:32 -0700

    finished styling main.jsx file

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,12 +10,12 @@ const Main = () => {
     return (
         <div id='main'>
             {/* <img className='w-full h-screen object-cover object-left' src={main_image} alt='' /> */}
-            <img className='w-full h-screen object-cover object-left scale-x-[-1]' src={main_alt} alt='' />
-            {/* <img className='w-full h-screen object-cover object-left ' src={main_alt2} alt='' /> */}
+            {/* <img className='w-full h-screen object-cover object-left scale-x-[-1]' src={main_alt} alt='' /> */}
+            <img className='w-full h-screen object-cover object-left ' src={main_alt2} alt='' />
             <div className='w-full h-screen absolute top-0 left-0 bg-white/50'>
-                <div>
-                    <h1>I'm Stephanie Puertas</h1>
-                    <h2>I'm a 
+                <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center'>
+                    <h1 className='sm:text-5xl text-4xl font-bold text-gray-800 '>I'm Stephanie Puertas</h1>
+                    <h2 className='flex sm:text-3xl text-2xl pt-4 text-gray-800'>I'm a 
                         <TypeAnimation
                             sequence={[
                                 'Developer', // Types 'One'
@@ -31,7 +31,7 @@ const Main = () => {
                             style={{ fontSize: '1em', display: 'inline-block', paddingLeft:'5px'}}
                         />
                     </h2>
-                    <div>
+                    <div className='flex justify-between pt-6 max-w-[200px] w-full '>
                         <FaTwitter className='cursor-pointer' size={20}/>
                         <FaFacebook className='cursor-pointer' size={20}/>
                         <FaInstagram className='cursor-pointer' size={20}/>
