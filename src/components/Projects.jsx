8280ec72d9f73704f40eb975a406e23283d998bd
stephaniepuertas <puertasstephanie@gmail.com>
2023-04-03 16:52:16 -0700

    finished adding projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,7 +11,7 @@ const Projects = () => {
     <div id='projects' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
       <h1 className='text-4xl font-bold text-center text-[#001b5e]'>Projects</h1>
       <p className='text-center py-8'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, aliquam vitae ut unde eius, itaque ratione quaerat quae fugiat minima beatae. Placeat dolor optio corporis ipsum suscipit nisi recusandae culpa.</p>
-      <div className='grid sm:grid-col-2 gap-12'>
+      <div className='grid sm:grid-cols-2 gap-12'>
         <Projectitem img={caviarlife} title='CaviarLife App'/>
         <Projectitem img={hoobank} title='HooBank App'/>
         <Projectitem img={myclick} title='MyClick App'/>
--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -12,7 +12,7 @@ const Sidenav = () => {
 
     return (
         <div>
-            <AiOutlineMenu onClick={handleNav} className='absolute top-4 right-4 z-[99] md:hidden'/>
+            <AiOutlineMenu onClick={handleNav} className='absolute top-4 right-4 z-[99] md:hidden mr-2'/>
 
             {/* CODE FOR MOBILE RESPONSE */}
             {/* TURNERY OPERATOR: if nav is true  */}
@@ -48,7 +48,7 @@ const Sidenav = () => {
                 )
             }
             {/* CODE FOR REGULAR NAVBAR */}
-            <div className='md:block hidden fixed top-[25%] z-10'>
+            <div className='md:block hidden fixed top-[25%] z-10 '>
                 <div className='flex flex-col'>
                     <a href='#main' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 ml-5 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
                     <AiOutlineHome size={20} />
