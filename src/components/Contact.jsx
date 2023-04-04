c264958abfff3843760ae7fa15d1354f5a9e15d4
stephaniepuertas <puertasstephanie@gmail.com>
2023-04-03 17:43:44 -0700

    added contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,30 +4,42 @@ const Contact = () => {
     return (
         <div id='contact' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
             <h1 className='py-4 text-4xl font-bold text-center text-[#fbaebd]'>Contact</h1>
-            <form action='' method='POST' encType='multipart/form-data' >
+            <form
+                name='contact'
+                method='post'
+                data-netlify='true'
+                netlify-honeypot="bot-field"
+                onSubmit='submit'
+            >
+                <input type='hidden' name='form-name' value='contact' />
+                <p class="hidden">
+                    <label>
+                        Don’t fill this out if you’re human: <input name="bot-field" />
+                    </label>
+                </p>
                 <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
                     <div className='flex flex-col'>
-                        <label className='uppercase text-sm py-2'>Name </label>
-                        <input className='border-2 rounded-lg p-3 flex border-gray-300' type="text" name='name' />
+                        <label htmlFor='name' className='uppercase text-sm py-2'>Name </label>
+                        <input  required  className='border-2 rounded-lg p-3 flex border-gray-300' type="text" name='name' id='name'/>
                     </div>
                     <div className='flex flex-col'>
-                        <label className='uppercase text-sm py-2'>Phone </label>
-                        <input className='border-2 rounded-lg p-3 flex border-gray-300' type="text" name='phone' />
+                        <label htmlFor='phone' className='uppercase text-sm py-2'>Phone </label>
+                        <input required  className='border-2 rounded-lg p-3 flex border-gray-300' type="text" name='phone' id='phone'/>
                     </div>
                 </div>
                 <div className='flex flex-col py-2'>
-                    <label className='uppercase text-sm py-2'>Email </label>
-                    <input className='border-2 rounded-lg p-3 flex border-gray-300' type="email" name='email' />
+                    <label htmlFor='email' className='uppercase text-sm py-2'>Email </label>
+                    <input required  className='border-2 rounded-lg p-3 flex border-gray-300' type="email" name='email' id='email'/>
                 </div>
                 <div className='flex flex-col py-2'>
-                    <label className='uppercase text-sm py-2'>Subject </label>
-                    <input className='border-2 rounded-lg p-3 flex border-gray-300' type="text" name='subject' />
+                    <label htmlFor='subject' className='uppercase text-sm py-2'>Subject </label>
+                    <input required  className='border-2 rounded-lg p-3 flex border-gray-300' type="text" name='subject' id='subject' />
                 </div>
                 <div className='flex flex-col py-2'>
-                    <label className='uppercase text-sm py-2'>Message </label>
-                    <textarea className='border-2 rounded-lg p-3 flex border-gray-300' rows='10' name='message'></textarea>
+                    <label htmlFor='message' className='uppercase text-sm py-2'>Message </label>
+                    <textarea required className='border-2 rounded-lg p-3 flex border-gray-300' rows='10' name='message' id='message'></textarea>
                 </div>
-                <button className='bg-[#fbaebd] text-gray-100 mt-4 w-full p-4 rounded-lg'>Send Message</button>
+                <button className='bg-[#fbaebd] text-gray-100 mt-4 w-full p-4 rounded-lg' type='submit'>Send Message</button>
 
             </form>
 
